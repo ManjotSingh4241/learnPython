33f55133b839a65c8ed912e7776d64cf8feb4637
Manjot singh <82448988+ManjotSingh4241@users.noreply.github.com>
2023-06-06 00:55:44 +0000

    Finished Excercise1

---
+++ day1/Excercise1.py
@@ -0,0 +1,8 @@
+#Calculator.....
+
+a = 6
+b = 9
+print("Sum of",a,  " and ",b, " :",  a+b)
+print("Substraction",a,  " and ",b, " :", a+b)
+print("Multiplication",a,  " and ",b, " :", a+b)
+print("Division",a,  " and ",b, " :", a+b)
