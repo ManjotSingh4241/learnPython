a83e2d5dacd81b03d485b83142d3dd1820c886f8
Manjot singh <82448988+ManjotSingh4241@users.noreply.github.com>
2023-06-06 00:43:32 +0000

    day1

--- day1/day1.py
+++ day1/day1.py
@@ -1,1 +1,13 @@
+# print("Hello World")
+# Escape sequence chanracter \n
+# print("Hello World\nI am Manjot Singh")
 
+# Variable and types
+a= 123
+print(a)
+b=True
+print(b)
+c = "Manjot"
+print(c)
+d = None
+print(d)
