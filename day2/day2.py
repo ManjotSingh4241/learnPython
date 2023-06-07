c27c4fbe0bdabde97d7f8c20d30380a0a73f5e3d
Manjot singh <82448988+ManjotSingh4241@users.noreply.github.com>
2023-06-07 00:35:24 +0000

    Day 2 Learning

--- day2/day2.py
+++ day2/day2.py
@@ -1,1 +1,29 @@
+#Functions...
 
+# def calculateGmean(a, b):
+#     mean = (a*b)/(a+b)
+#     print(mean)
+
+# def isGreater(a, b):
+#     if a>b:
+#         print("a is big")
+#     else:
+#         print("b is big")
+
+# def isLesser(a, b):
+#     pass
+
+# a = 9
+# b = 80
+
+# calculateGmean(a, b)
+
+# isGreater(a, b)
+
+#Function Arguments
+
+# def average(a=9, b=3):
+#     print("The average is: ", (a+b)/2)
+
+# average(4)#default b = 3
+# average(b=4)#default a = 9
